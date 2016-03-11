88a2e46736f9900734cfe23c37dddd03adc491b3
IanLondon <ianlondondu@gmail.com>
2016-03-11 18:17:23 -0500

    Form recieves input

--- tinylibrary/app.py
+++ tinylibrary/app.py
@@ -4,5 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tinylibrary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #XXX:???
+app.config.from_pyfile('secrets.py')
 
 db = SQLAlchemy(app)
--- tinylibrary/views.py
+++ tinylibrary/views.py
@@ -1,18 +1,40 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
+from flask_wtf import Form
+from wtforms import StringField, HiddenField, validators
 
 from app import app
 from models import Book, Room, Checkout
+
+#########
+# Forms #
+#########
+
+class AddBookForm(Form):
+    isbn = StringField(label='ISBN', validators=[validators.DataRequired()])
+    inside_cover_id = StringField(label=  'Custom ID')
+    title = StringField()
+    description = StringField()
+    thumbnail_url = StringField()
+
+#########
+# Views #
+#########
+
+@app.route('/add_books/', methods=['GET','POST'])
+def add_books():
+    add_book_form = AddBookForm()
+    if add_book_form.validate_on_submit():
+        print 'got add book form', add_book_form.isbn.data, add_book_form.inside_cover_id.data
+        return redirect((url_for('show_books')))
+    return render_template('add_books.html', form=add_book_form)
 
 @app.route('/books/')
 def show_books():
     return render_template('show_books.html', books=Book.query.all())
 
+
 # inside_cover_id is optional
-@app.route('/books/<isbn>', methods=['GET','POST'])
-@app.route('/books/<isbn>/<inside_cover_id>', methods=['GET','POST'])
+@app.route('/books/<isbn>')
+@app.route('/books/<isbn>/<inside_cover_id>')
 def single_book(isbn=None, inside_cover_id=None):
-    print 'DEBUG: got single book ', isbn, inside_cover_id
-    if request.method == 'GET':
-        return 'GET not implemented!'
-    if request.method == 'POST':
-        return 'Post not implemented!'
+    return 'GET not implemented!'
